787ae034baa2995921294cf1018df68dacf43d84
Nitheesh D R <nitheeraj1@gmail.com>
2025-12-08 20:48:24 +0530

    Update index.tsx

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,10 +1,10 @@
-import { Link } from "expo-router";
-import { Text, View } from "react-native";
+import { images } from "@/constants/images";
+import { Image,Text, View } from "react-native";
  
 export default function App() {
   return (
-    <View className="flex-1 bg-primary">
-
+    <View className="flex-1 bg-primary ">
+      <Image source={images.bg} className="absolute w-full z-0" />
     </View>
   );
 }
