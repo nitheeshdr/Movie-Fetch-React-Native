cc18d2ea0a918965afd726df704db742d537200d
Nitheesh D R <nitheeraj1@gmail.com>
2025-12-08 22:08:16 +0530

    add components

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -7,15 +7,15 @@ interface Props{
     onPress?: ()=> void;
 }
 
-const SearchBar = ({placeholder,onPress}) => {
+const SearchBar: React.FC<Props>= ({placeholder,onPress}) => {
   return (
     <View className='flex-row items-center bg-dark-200 rounded-full px-5 py-4 '>
       <Image source={icons.search} className='size-5' resizeMode='contain' tintColor='#ab8bff' />
       <TextInput 
-        onPress={()=>{}}
-        placeholder='Search'
+        onPress={onPress}
+        placeholder={placeholder}
         value='' 
-        onChangeText={()=> {}}
+        onChangeText={()=>{}}
         placeholderTextColor="#a8b5db" 
         className='flex-1 ml-2 text-white'/>
     </View>
