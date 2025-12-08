9a7ea62384d6883c11c2d5aea01a35ce0283bed1
Nitheesh D R <nitheeraj1@gmail.com>
2025-12-08 20:45:07 +0530

    first

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,10 @@
+import { Link } from "expo-router";
+import { Text, View } from "react-native";
+ 
+export default function App() {
+  return (
+    <View className="flex-1 bg-primary">
+
+    </View>
+  );
+}
