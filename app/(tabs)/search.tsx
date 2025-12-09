5ab34a086111a6af28384b10c2ba56a550ec8d57
Nitheesh D R <nitheeraj1@gmail.com>
2025-12-09 14:48:12 +0530

    search screen

--- app/(tabs)/search.tsx
+++ app/(tabs)/search.tsx
@@ -1,9 +1,43 @@
-import { View, Text } from 'react-native'
+import MovieCard from '@/components/MovieCard'
+import { images } from '@/constants/images'
+import { fetchMovies } from '@/services/api';
+import useFetch from '@/services/usefetch';
+import { useRouter } from 'expo-router';
+import { View, Text,Image, FlatList } from 'react-native'
 
 const search = () => {
+  
+  const router = useRouter();
+  const {
+    data: movies,
+    loading: moviesLoading,
+    error: moviesError,
+  } = useFetch(() =>
+    fetchMovies({
+      query: '',
+    })
+  );
   return (
     <View className='flex-1 bg-primary'>
-      <Text className='text-black'>search</Text>
+      <Image source={images.bg} className='flex-1 w-full absolute z-0' resizeMode='cover'/>
+
+      <FlatList 
+        data={movies} 
+        renderItem={({ item }) => <MovieCard {...item}/> }
+        keyExtractor={(item)=> item.id.toString()}
+        className='px-5 '
+        numColumns={3}
+        columnWrapperStyle={{
+          justifyContent:'center',
+          gap: 16,
+          marginVertical:16,
+
+        }}
+        contentContainerStyle={{
+          paddingBottom:100
+        }}
+        
+        />
     </View>
   )
 }
